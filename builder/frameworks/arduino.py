2dfccc4bed8bbbeddc9de1ba4fdf35b8b65bbf66
qitas <qitas@qitas.cn>
2019-09-19 14:05:07 +0800

    add src

---
+++ builder/frameworks/arduino.py
@@ -0,0 +1,10 @@
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+platform = "arduino"
+module = platform + "-" + env.BoardConfig().get("build.core")
+m = __import__(module)       
+globals()[module] = m
+m.dev_init(env, platform)
+#print env.Dump()
+
